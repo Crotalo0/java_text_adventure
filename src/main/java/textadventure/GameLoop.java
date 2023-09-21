c780f5d18594104d453bb2a31392399a6823f4f3
Riccardo Marini <marini.riccardo97@gmail.com>
2023-09-21 17:17:30 +0200

    modificato secondo print

--- src/main/java/textadventure/GameLoop.java
+++ src/main/java/textadventure/GameLoop.java
@@ -3,7 +3,7 @@ package textadventure;
 public class GameLoop {
     public static void main(String[] args) {
         System.out.println("ciao");
-        System.out.println("secondo ciao");
+        System.out.println("secondo ciao, nella fake branch");
         System.out.println("Terzo ciao, ma modificato");
     }
 }
