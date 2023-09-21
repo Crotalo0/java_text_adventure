c99514c33614a874955107f6f48c2166c36ecadb
Riccardo Marini <marini.riccardo97@gmail.com>
2023-09-21 17:08:37 +0200

    modificato terzo print

--- src/main/java/textadventure/GameLoop.java
+++ src/main/java/textadventure/GameLoop.java
@@ -4,7 +4,7 @@ public class GameLoop {
     public static void main(String[] args) {
         System.out.println("ciao");
         System.out.println("secondo ciao");
-        System.out.println("Terzo ciao");
+        System.out.println("Terzo ciao, ma modificato");
     }
 }
 
