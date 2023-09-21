9619d72a3526302ccea83ed0d722ea8fde7dea04
Alessio Di Stasi <adistasi18@gmail.com>
2023-09-21 12:12:21 +0200

    Add third ciao

--- src/main/java/textadventure/GameLoop.java
+++ src/main/java/textadventure/GameLoop.java
@@ -4,6 +4,7 @@ public class GameLoop {
     public static void main(String[] args) {
         System.out.println("ciao");
         System.out.println("secondo ciao");
+        System.out.println("Terzo ciao");
     }
 }
 
