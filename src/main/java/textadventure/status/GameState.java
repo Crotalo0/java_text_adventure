fbfad7f4d2b213a72abacf181e1b36e185dfa04a
Alessio Di Stasi <adistasi18@gmail.com>
2023-09-23 00:54:55 +0200

    Add temporary placeholder for player position info

--- src/main/java/textadventure/status/GameState.java
+++ src/main/java/textadventure/status/GameState.java
@@ -1,2 +1,15 @@
-package textadventure.status;public class GameState {
+package textadventure.status;
+
+public class GameState {
+    private String playerLocation;
+
+    public String getPlayerLocation() {
+        return playerLocation;
+    }
+
+    public void setPlayerLocation(String location) {
+        playerLocation = location;
+    }
+
+    // Other game state information and methods...
 }
