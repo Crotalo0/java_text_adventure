78fa71cd5c3c284f3742ad34bf1b8d8913c91f1f
Alessio Di Stasi <adistasi18@gmail.com>
2023-09-23 00:28:51 +0200

    Changed .next with .nextLine

---
+++ src/main/java/textadventure/utils/InputParser.java
@@ -0,0 +1,11 @@
+package textadventure.utils;
+// There will live the input validation of our project
+// 1. Split user input in individual words
+// 2. convert all to lowercase
+// 3. search for a valid word inside
+// 4. search for eventual parameters
+// 5. Error handling
+// 6. Organize the command to be digested by the program somewhere else
+
+public class InputParser {
+}
