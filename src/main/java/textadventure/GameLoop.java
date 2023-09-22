67314f65a427ef8ac560a5098f6583bfc40f2aff
Alessio Di Stasi <adistasi18@gmail.com>
2023-09-23 00:56:50 +0200

    Add command test

--- src/main/java/textadventure/GameLoop.java
+++ src/main/java/textadventure/GameLoop.java
@@ -2,7 +2,11 @@ package textadventure;
 
 import textadventure.chapters.Chapter1;
 import textadventure.characters.Player;
+import textadventure.commands.CommandProcessor;
+import textadventure.commands.GoCommand;
+import textadventure.commands.LookCommand;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class GameLoop {
@@ -10,6 +14,7 @@ public class GameLoop {
 
         // Scanner object creation for INPUT
         Scanner s = new Scanner(System.in);
+
         // What INPUT wants?
         System.out.println("Enter player name: ");
         // Method of Scanner object to retrieve last console INPUT
@@ -17,11 +22,26 @@ public class GameLoop {
 
         // Create player
         Player player1 = new Player(name);
-
-        // Create Chapter1
         Chapter1 chapter1 = new Chapter1(player1);
-        // Call chapter1 method .start()
         chapter1.start();
+
+        // Command processing test
+        CommandProcessor commandProcessor = new CommandProcessor();
+        // Fill dicts with commands
+        commandProcessor.registerCommand("look", new LookCommand());
+        commandProcessor.registerCommand("go", new GoCommand("north"));
+
+
+        String playerInput;
+        do {
+            System.out.print("Enter your command: ");
+            playerInput = s.nextLine();
+
+            // Parse and execute the command
+            commandProcessor.executeCommand(playerInput);
+        }
+        while(!Objects.equals(playerInput, "stop"));
+
 
     }
 }
