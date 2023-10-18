1b3b8f7f6e58b97d79a3a0a3fb50f859ba46b034
Alex <adistasi18@gmail.com>
2023-10-19 00:40:05 +0200

    Add class in which we define all commands

---
+++ src/main/java/com/textadventure/chapters/CommandInitializer.java
@@ -0,0 +1,20 @@
+package com.textadventure.chapters;
+
+import com.textadventure.commands.*;
+import com.textadventure.status.GameState;
+
+public class CommandInitializer extends CommandProcessor {
+
+    public CommandInitializer(GameState gameState) {
+        // Add there all available commands
+        this.registerCommand("look", new Look(gameState));
+        this.registerCommand("go", new Go(gameState));
+        this.registerCommand("move", new Go(gameState));
+        this.registerCommand("stop", new Stop(gameState));
+        this.registerCommand("help", new Help(gameState));
+        this.registerCommand("status", new Status(gameState));
+        this.registerCommand("attack", new Attack(gameState));
+        this.registerCommand("ability", new SuperAttack(gameState));
+    }
+
+}
