5be67b46269974cc4f130450481fbff1ada83480
Crotalo0 <adistasi18@gmail.com>
2023-11-22 18:59:56 +0100

    Edit .gitignore

--- src/main/java/com/textadventure/utils/GameLoop.java
+++ src/main/java/com/textadventure/utils/GameLoop.java
@@ -1,8 +1,11 @@
 package com.textadventure.utils;
 
+import com.textadventure.characters.entities.CharacterEntity;
 import com.textadventure.commands.handling.CommandInitializer;
 import com.textadventure.status.GameState;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class GameLoop {
@@ -20,6 +23,22 @@ public class GameLoop {
             System.out.print("Enter your command: ");
             playerInput = ScannerSingleton.getInstance().nextLine();
             CommandInitializer.getInstance().parseAndExecuteCommand(playerInput);
+            GameState gs = GameState.getInstance();
+
+            // TODO: refactor in another class
+            if (!gs.getPlayer().isAlive()) {
+                System.out.println("You died!");
+            }
+            List<CharacterEntity> deadEnemies = new ArrayList<>();
+            List<CharacterEntity> aliveEnemies = gs.getEnemies();
+            for (CharacterEntity enemy : gs.getEnemies()) {
+                if (!enemy.isAlive()) {
+                    System.out.println(enemy.getName() + " has died!");
+                    deadEnemies.add(enemy);
+                }
+            }
+            aliveEnemies.removeIf(deadEnemies::contains);
+            gs.setEnemies(aliveEnemies);
 
         } while (!Objects.equals(playerInput, "stop"));
     }
